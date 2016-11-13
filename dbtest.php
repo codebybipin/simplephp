e7484793f3fcea8d9464f8152d529037d9324026
codebybipin <bipingiri99@gmail.com>
2016-11-13 15:45:47 +0545

    Update dbtest.php

--- dbtest.php
+++ dbtest.php
@@ -14,13 +14,14 @@ echo "<br><br><br><br>My awesome PHP test<br>";
 $query = "SELECT * from user";
 $rs = $connection->query($query);
 while ($row = $rs->fetch_array(MYSQLI_ASSOC)) {
-    echo $row['id'] . " " . $row['name'] . "<br>";
-    echo $dbhost . "<br>";
+    echo $row['userID'] . " " . $row['username'] . "<br>";
+    
+}
+echo $dbhost . "<br>";
     echo $dbport . "<br>";
     echo $dbuser . "<br>";
     echo $dbpwd . "<br>";
     echo $dbname . "<br>";
-}
 $rs->close();
 $connection->close();
 ?>
