c5d73e3ede4eed4a8be15be1ef0ffcab7b1f39c6
codebybipin <bipingiri99@gmail.com>
2016-11-15 15:47:00 +0545

    Update index.php

--- index.php
+++ index.php
@@ -3,3 +3,13 @@
 echo "Welcome to the best application in the world.";
 
 echo "<br>All things open is cool";
+$html = file_get_html('http://www.google.com/');
+
+// Find all images
+foreach($html->find('img') as $element)
+       echo $element->src . '<br>';
+
+// Find all links
+foreach($html->find('a') as $element)
+       echo $element->href . '<br>';
+?>
